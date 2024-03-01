c4be1704542b8aeec00cee40b5cc9a144219c57d
Rodolfo Sampaio <rodolfo_1202@hotmail.com>
2024-03-01 15:49:03 -0300

    feat: adicionar projeto TabelaFipe do Spring Initializr
    - Projeto inicializado usando o Spring Initializr com as configurações padrão.

---
+++ src/main/java/br/com/alura/TabelaFipe/TabelaFipeApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.TabelaFipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabelaFipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabelaFipeApplication.class, args);
+	}
+
+}
